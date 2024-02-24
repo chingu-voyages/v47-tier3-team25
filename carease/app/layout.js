b6d74b28ec2ca3249d1d07142fda11a031aee51c
jdx-code <jyotirmoydas12@gmail.com>
2024-02-24 15:37:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ carease/app/layout.js
@@ -0,0 +1,15 @@
+
+import './globals.css';
+
+export const metadata = {
+  title: 'CarEase',
+  description: 'Car Comparision Webapp',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ carease/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        supercar: 'url(/images/supercar.png)',
+        'signup-bg': 'url(/images/signup-bg.jpg)',
+        'carlist-hero': 'url(/images/carlist-hero.png)',
+        'fp-bg-car': 'url(/fp-bg-car.jpg)',
+        ellipse: 'url(/images/ellipse.svg)',
+        pattern: 'url(/images/pattern.png)',
+      },
+      colors: {
+        overlay_bg: '#14111169',
+        btn_blue: '#AACBED',
+      },
+    },
+  },
+  plugins: [],
+};
